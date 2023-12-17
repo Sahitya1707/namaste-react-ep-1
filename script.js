14648291e06a7e59148d7f3fbaa8d7b6643cb77f
Sahitya Neupane <neupanesahitya1@gmail.com>
2023-12-17 10:54:54 -0500

    namaste react

---
+++ script.js
@@ -0,0 +1,13 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement(
+      "div",
+      { id: "child" },
+
+      [
+        React.createElement("h1", {}, "I'm an h1 tag."),
+        React.createElement("h2", {}, "I'm an h2 tag."),
+      ]
+    ),
+  ]),
+]);
