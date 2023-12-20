d9c30d9ad80accfa3efc0e7eeda96d94f39ed41e
Sahitya Neupane <neupanesahitya1@gmail.com>
2023-12-20 14:09:22 -0500

    "namaste react episode 2"

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement(
@@ -6,10 +9,12 @@ const parent = React.createElement("div", { id: "parent" }, [
 
       [
         React.createElement("h1", {}, "I'm an h1 tag."),
+        React.createElement("h1", {}, "This is sahitya Neupane."),
         React.createElement("h2", {}, "I'm an h2 tag."),
       ]
     ),
   ]),
 ]);
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 #Namaste React
+
+#parcel
+
+- Dev Build
+- Local Server
+- HMR (Hot Module Replacement)
+- File Watching Algortihm - written in c++
+- Caching - Faster Builds
+- Image Optimization
+- Minification of file
+- Bundle all the files
+- Consitent Hashing
+- Code splitting
+- Differential Bundling - to support older browsers
+- Error Handling
+- HTTPS
+- Tree Shaking - Remove unused code s
