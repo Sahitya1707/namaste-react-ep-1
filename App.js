e8ccb53752ccef6f66064c4c8afc6d132360dfd3
Sahitya Neupane <neupanesahitya1@gmail.com>
2023-12-23 11:03:52 -0500

    episode-3 Laying the Foundation (Namaste React)

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement(
-      "div",
-      { id: "child" },
+// React Element => Object
 
-      [
-        React.createElement("h1", {}, "I'm an h1 tag."),
-        React.createElement("h1", {}, "This is sahitya Neupane."),
-        React.createElement("h2", {}, "I'm an h2 tag."),
-      ]
-    ),
-  ]),
-]);
-console.log(parent);
+// const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
+
+//JSX (transpiled before it reaches the JS - PARCEL - BABEL)
+
+// JSX = React.createElement => ReactElement - JS Object = HtmlElement (render)
+const elem = <span>React Element</span>;
+const heading = <h1 id="heading">Namaste React using JSX.</h1>;
+
+// React Component
+const body = (
+  <h1 className="body">
+    {elem}
+    This is body
+  </h1>
+);
+
+// Functional Based Component - NEW
+const Title = () => <h1 className="head">Namaste React By Sahitya</h1>;
+
+// Component Composition
+const HeadingComponent = () => {
+  return (
+    <>
+      <div className="container">
+        {body}
+        <h1 className="heading">Namaste React Function Component</h1>
+        <Title />
+      </div>
+    </>
+  );
+};
+
+// console.log(heading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
