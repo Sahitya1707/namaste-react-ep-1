9d64c305ca5dd73bce7ac6ef14333dbd261c80dd
Sahitya Neupane <neupanesahitya1@gmail.com>
2023-12-27 08:44:32 -0500

    episode 4 part 1 completed

--- App.js
+++ App.js
@@ -1,41 +1,90 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Element => Object
+/*
+* Header 
+  - Logo
+  - Nav Items
 
-// const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
 
-//JSX (transpiled before it reaches the JS - PARCEL - BABEL)
+*Body
+  - Search
+  - CardContainer
+    - Cards
+      Img
+      Name of Res, Star Rating, Cusine, Delivery time
+   
 
-// JSX = React.createElement => ReactElement - JS Object = HtmlElement (render)
-const elem = <span>React Element</span>;
-const heading = <h1 id="heading">Namaste React using JSX.</h1>;
+*Footer
+ - CopyRight
+ - Links
+ - Address
+ - Contact
 
-// React Component
-const body = (
-  <h1 className="body">
-    {elem}
-    This is body
-  </h1>
-);
+*/
 
-// Functional Based Component - NEW
-const Title = () => <h1 className="head">Namaste React By Sahitya</h1>;
-
-// Component Composition
-const HeadingComponent = () => {
+const Header = () => {
   return (
-    <>
-      <div className="container">
-        {body}
-        <h1 className="heading">Namaste React Function Component</h1>
-        <Title />
+    <div className="header">
+      <div>
+        <img
+          src="https://png.pngtree.com/png-clipart/20230106/original/pngtree-simple-and-modern-food-logo-vector-design-png-image_8876455.png"
+          alt=""
+          className="logo"
+        />
       </div>
-    </>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const RestaurantCard = () => {
+  return (
+    <div className="res-card">
+      <img
+        src="https://i.ytimg.com/vi/7uyOcS8QCTg/maxresdefault.jpg"
+        alt="megna-food"
+        className="res-logo"
+      />
+      <h3>Megna Food</h3>
+      <h4>Biryani, North Indian, Nepali</h4>
+      <h4>4.5 Rating</h4>
+      <h4>38 Minutes</h4>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-container">
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
   );
 };
 
 // console.log(heading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
